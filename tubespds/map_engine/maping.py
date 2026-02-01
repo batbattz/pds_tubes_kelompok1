4d5f9b1df49abddd14c33df34fefbed3101e7309
batbattz <ubaidahakbar2396@gmail.com>
2026-02-02 00:50:40 +0700

    Update maping.py

--- tubespds/map_engine/maping.py
+++ tubespds/map_engine/maping.py
@@ -58,14 +58,14 @@ def create_choropleth_map(df_final, geojson):
     return m
 @st.cache_data
 def load_geojson(relative_path):
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_dir,"..", "Jabar_Kec_clean.json")
+    base_path = os.getcwd()
+    full_path = os.path.join(base_path, relative_path)
     
-    with open(file_path, "r", encoding="utf-8") as f:
+    with open(full_path, "r", encoding="utf-8") as f:
         return json.load(f)
 
-
 # m.save("peta_sppg4.html")
+
 
 
 
